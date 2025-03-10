e6d9fdc4941ec47480146e9bd307bb33c7ec98f2
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-10 00:48:53 -0300

    feat: add initial config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{tsx,ts}'],
+  presets: [[require('nativewind/preset')]],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
