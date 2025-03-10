8cb7beb554c0caed171dc71e7984413963d9e9e1
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-10 01:47:41 -0300

    feat: add content to the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{tsx,ts}'],
+  content: ['./app/**/*.{tsx,ts,js,jsx}', './app/*.{tsx, ts, js, jsx}'],
   presets: [[require('nativewind/preset')]],
   theme: {
     extend: {},
