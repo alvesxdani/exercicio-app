aeaa06e473bb3a3557286d71423e50738997c7cc
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-17 23:00:35 -0300

    feat: add noto sans font in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./app/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        notosans: ['NotoSans-Regular'],
+      },
+    },
   },
   plugins: [],
 }
