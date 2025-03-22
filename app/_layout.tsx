bb5f31b14a84a84a3583a62b80a116c27dbd6a9e
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-22 20:37:13 -0300

    feat: add gluestack installation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
+import '@/global.css'
 import { Stack } from 'expo-router'
 import '../global.css'
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <GluestackUIProvider mode="light">
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </GluestackUIProvider>
   )
 }
