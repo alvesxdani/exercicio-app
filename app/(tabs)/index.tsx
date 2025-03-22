d7bbb108a396fbb3409cd20665f600a416b88570
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-22 20:23:23 -0300

    feat: add noto sans class

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 export default function Home() {
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
-      <Text className="text-purple-500">Página inicial</Text>
+      <Text className="text-purple-500 font-notosans">Página inicial</Text>
     </SafeAreaView>
   )
 }
