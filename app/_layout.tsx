c1ba30dc66aad255e1e83f65994605aa6bb9cd52
Daniela Alves <dani.alvesm22@gmail.com>
2025-03-23 00:15:37 -0300

    refactor: modify the structure to single page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <GluestackUIProvider mode="light">
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </GluestackUIProvider>
   )
